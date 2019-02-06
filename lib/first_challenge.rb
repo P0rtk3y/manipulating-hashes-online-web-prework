1192a97dd320ba5a02be9d2db7084935cda36eac
Lana Worobec <llama.p0rtk3y@gmail.com>
2019-02-06 08:50:50 +0000

    Automatically backed up by Learn

--- lib/first_challenge.rb
+++ lib/first_challenge.rb
@@ -14,7 +14,7 @@ def first_challenge
   }
 
   #your code here
-  contacts["Freddy Mercury"].each do 
+  contacts["Freddy Mercury"].each{|k,v| 
   
   #remember to return your newly altered contacts hash!
   contacts
