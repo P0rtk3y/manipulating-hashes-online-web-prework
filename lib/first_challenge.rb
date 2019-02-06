50090f45fb52034fe41549a9a38bbba6d11ca59f
Lana Worobec <llama.p0rtk3y@gmail.com>
2019-02-06 08:51:45 +0000

    Automatically backed up by Learn

--- lib/first_challenge.rb
+++ lib/first_challenge.rb
@@ -16,6 +16,7 @@ def first_challenge
   #your code here
   contacts["Freddy Mercury"].each do |k,v| 
     if k == :favorite_icecream_flavors
+      v.delete_if
   
   #remember to return your newly altered contacts hash!
   contacts
