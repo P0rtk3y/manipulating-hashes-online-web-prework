8a144ce312d12a0ebfa0e69ef92398f79f63b75d
Lana Worobec <llama.p0rtk3y@gmail.com>
2019-02-06 08:50:08 +0000

    Automatically backed up by Learn

--- lib/first_challenge.rb
+++ lib/first_challenge.rb
@@ -14,7 +14,8 @@ def first_challenge
   }
 
   #your code here
-  contacts["Freddy Mercury"][:favorite_icecream_flavors].each {|flavor| [:favorite_icecream_flavors].delete_if flavor == "strawberry" }
+  contacts["Freddy Mercury"]
+  
   #remember to return your newly altered contacts hash!
   contacts
 end
